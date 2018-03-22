856e04d010bafe00d0e45545f5bdf10e9478332f
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2018-03-22 13:30:20 -0500

    changing the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,11 +8,21 @@ class Footer extends React.Component {
 		return (
 			<footer className="pw-footer">
 				<section className="pw-content">
-					<section className="pw-section">
-
+					<h5>Primer proyecto para Programación en la web</h5>
+					<ul className="pw-list">
+						<li>Juan Miguel Gomez Ganem</li>
+						<li>Juan Manuel Sánchez</li>
+						<li>Luis David Zárate Castillo</li>
+					</ul>
+				</section>
+				<section className="pw-copyright">
+					<span>{copyright}</span>
+					<section>
+						<a href="https://github.com/juanmsl/pw-library-front" className="pwi pwi-left pwi-github">Frontend</a>
+						<span className="pw-separator">|</span>
+						<a href="https://github.com/juanmsl/pw-library-api" className="pwi pwi-left pwi-github">Backend</a>
 					</section>
 				</section>
-				<section className="pw-copyright">{copyright}</section>
 			</footer>
 		);
 	}
