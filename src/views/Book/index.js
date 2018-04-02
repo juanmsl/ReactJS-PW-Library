4d05f6dc2c5cb0609f9495b08baf8dde3183504e
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2018-04-01 22:27:35 -0500

    creatig branch for book_view

--- src/views/Book/index.js
+++ src/views/Book/index.js
@@ -14,47 +14,48 @@ class Book extends React.Component{
 			selectedIDs: [],
 			selected: [],
 			newAuthors: [],
-			gettingBooks: "pending"
-		}
+			gettingAuthors: "pending"
+		};
 		this.restResolver = new RESTResolver();
 	};
 
-	getAuthors = (books) =>{
-		const authors = [];
-		const authIDs = [];
-		books.forEach((book)=>{
-			const { autores } = book;
-			autores.forEach((autor)=>{
-				if( !authIDs.includes(autor.id) ){
-					authors.push(autor);
-					authIDs.push(autor.id);
-				}
-			})
-		})
-		return authors;
-	}
-
 	componentWillMount = () => {
-		this.restResolver.getBooks((response) => {
+		this.restResolver.getAuthors((response) => {
 			this.setState({
-				autores: this.getAuthors(response),
-				available: this.getAuthors(response).map((autor) => {return autor.nombre}),
-				gettingBooks: 'success'
+				autores: response,
+				available: response.map((autor) => {return autor.nombre}),
+				gettingAuthors: 'success'
 			});
 		}, (response) => {
+			let autores = [
+				{
+					id: 1,
+					nombre: "Juan"
+				},
+				{
+					id: 2,
+					nombre: "Luis"
+				},
+				{
+					id: 3,
+					nombre: "Carlos"
+				}
+			];
 			this.setState({
-				gettingBooks: 'error'
+				autores: autores,
+				available: autores.map((autor) => {return autor.nombre}),
+				gettingAuthors: 'error'
 			});
 		});
-	}
+	};
 
-	handleSubmit = (e,state) =>{
+	handleSubmit = (e, state) =>{
 		const { newAuthors , selected } = this.state;
 		const selectedAuths = newAuthors.length > 0 ? selected.concat(newAuthors) : selected;
 		const values = {
 			...state,
 			autores: selectedAuths
-		}
+		};
 		console.log(values);
 		console.log("Please implement submit method");
 	};
@@ -63,30 +64,30 @@ class Book extends React.Component{
 		const { autores } = this.state;
 		const aval = autores.filter((autor) => {
 			return !ids.includes(autor.id)
-		})
+		});
 		return aval.map((item) => {return item.nombre})
-	}
+	};
 
-	updateLists = (selectedIDs,newAuthors) =>{
+	updateLists = (selectedIDs, newAuthors) =>{
 		const { autores } = this.state;
 		let newSelected = selectedIDs.map((id) => {
 			return autores.find((autor) => { return autor.id === id })
-		}).map((a) => {return a.nombre})
+		}).map((a) => {return a.nombre});
 		this.setState({
 			selectedIDs: selectedIDs,
 			selected: newSelected,
 			available: this.getFilteredAvailable(selectedIDs),
 			newAuthors: newAuthors
 		})
-	}
+	};
 
 	addAuthor = (e,autor) =>{
 		const { index } = autor;
 		const { selectedIDs , available , autores , newAuthors } = this.state;
-		const author = autores.find((a) => { return a.nombre === available[index] })
+		const author = autores.find((a) => { return a.nombre === available[index] });
 		selectedIDs.push( author.id );
 		this.updateLists(selectedIDs,newAuthors);
-	}
+	};
 
 	removeAuthor = (e,autor) =>{
 		const { index } = autor;
@@ -94,17 +95,17 @@ class Book extends React.Component{
 		const auth = selected[index];
 		const found = autores.find((autor) => {
 			return autor.nombre === auth;
-		})
+		});
 		if( found === undefined ){
 			newAuthors.splice(selectedIDs.indexOf(auth),1);
 		}else{
 			selectedIDs.splice(selectedIDs.indexOf(found.id),1);
 		}
 		this.updateLists(selectedIDs,newAuthors)
-	}
+	};
 
 	handleNewAuthor = (e,state) =>{
-		let { newAuthor="" } = state;
+		let { newAuthor = "" } = state;
 		newAuthor = newAuthor.trim();
 		if( newAuthor !== "" ){
 			const { autores , selectedIDs } = this.state;
@@ -113,11 +114,11 @@ class Book extends React.Component{
 				const { newAuthors } = this.state;
 				if( !newAuthors.includes(newAuthor) ){
 					newAuthors.push(newAuthor);
-					this.updateLists(selectedIDs,newAuthors);
+					this.updateLists(selectedIDs, newAuthors);
 				}
 			}
 		}
-	}
+	};
 
 	render() {
 		const { data, user } = this.props;
@@ -134,23 +135,26 @@ class Book extends React.Component{
 				<section className="pw-form-container">
 					<Form onSubmit={handleSubmit} className="pw-form" autocomplete="off">
 						<Field>
-							<Input id="title-input" name="title" placeholder="Titulo" className="pw-input"  required={true} />
-							<Label id="title-label" htmlFor="title-input" className="pw-label pwi pwi-user"/>
+							<Input id="title-input" name="title" placeholder="Titulo" className="pw-input" required={true} />
+							<Label id="title-label" htmlFor="title-input" className="pw-label pwi pwi-book"/>
 						</Field>
 						<Field>
-							<Input id="isbn-input" name="isbn" placeholder="ISBN" className="pw-input"  required={true} />
-							<Label id="isbn-label" htmlFor="isbn-input" className="pw-label pwi pwi-user"/>
+							<Input id="isbn-input" name="isbn" placeholder="ISBN" className="pw-input" required={true} />
+							<Label id="isbn-label" htmlFor="isbn-input" className="pw-label pwi pwi-qrcode"/>
 						</Field>
+						<section>
+							<h3>Autores</h3>
+						</section>
 						<Label>Autores</Label>
 						<List onClick={addAuthor} items={available} emptyMessage={emptyAuthors}/>
 						<Label>Selected</Label>
 						<List onClick={removeAuthor} items={selectedAuths} emptyMessage={emptySelected}/>
 						<Form onSubmit={handleNewAuthor}>
 							<Field>
-								<Input name="newAuthor"/>
-								<Label>Nuevo</Label>
-								<Button submit>Add</Button>
+								<Input id="autor-input" name="autor" placeholder="Autor" className="pw-input"/>
+								<Label id="autor-label" htmlFor="autor-input" className="pw-label pwi pwi-users" />
 							</Field>
+							<Button submit>Add</Button>
 						</Form>
 						<Button submit className="pw-submit wh-button active shadow">Agregar libro</Button>
 					</Form>
